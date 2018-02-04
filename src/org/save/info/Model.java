e57a276a56b9b7747966adda5b1b3a45faa8288d
RayMiles <32762853+RayMiles94@users.noreply.github.com>
2018-02-04 20:36:23 +0100

    remove controle object

--- src/org/save/info/Model.java
+++ src/org/save/info/Model.java
@@ -5,14 +5,11 @@ import java.io.FileNotFoundException;
 public class Model {
   View v;
   Preson p;
-  Controle c;
   public Model(){
 	  v = new View("App");
   }
-  
   public void ShowDATA() throws FileNotFoundException{
 	  p = new Preson();
-	  c = new Controle(v.tn.getText(),v.tp.getText());
 	  v.Update("SAVED","NOT SAVED",p);
   }
 }
