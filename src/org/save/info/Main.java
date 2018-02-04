bcd938c1822e37cbc935c0a18790a06c6650310e
RayMiles <32762853+RayMiles94@users.noreply.github.com>
2018-02-04 20:26:29 +0100

    add main java file
    content main class for application

---
+++ src/org/save/info/Main.java
@@ -0,0 +1,8 @@
+package Org.save.Info;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+		new Model().ShowDATA();} 
+}
