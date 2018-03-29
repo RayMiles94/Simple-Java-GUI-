33627c02ede64b8e496c7c5325fbb23886640430
RayMiles <32762853+RayMiles94@users.noreply.github.com>
2018-03-29 13:26:53 +0200

    remove Object person at line 9

--- src/org/save/info/Model.java
+++ src/org/save/info/Model.java
@@ -6,7 +6,6 @@ public class Model {
 	  v = new View("App");
   }
   public void ShowDATA() throws FileNotFoundException{
-	  p = new Preson();
-	  v.Update("SAVED","NOT SAVED",p);
+	  v.Update("SAVED","NOT SAVED",new Person());
   }
 }
