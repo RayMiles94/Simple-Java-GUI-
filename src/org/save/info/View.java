2799e1e225a1245eec9e5a5507c05dfaeb606786
RayMiles <32762853+RayMiles94@users.noreply.github.com>
2018-07-12 17:05:11 +0200

    add license

--- src/org/save/info/View.java
+++ src/org/save/info/View.java
@@ -1,3 +1,15 @@
+/**
+	Project: simple java gui
+	author : ray miles
+	license : GPL
+	The GNU General Public License is a free, copyleft license for software and other kinds of works.
+        The licenses for most software and other practical works are designed to take away your freedom to 
+	share and change the works. By contrast, the GNU General Public License is intended to guarantee your 
+	freedom to share and change all versions of a program--to make sure it remains free software for all its 
+	users. We, the Free Software Foundation, 
+	use the GNU General Public License for most of our software; it applies also to any other work released this way by its authors.
+	You can apply it to your programs, too.
+*/
 package Org.save.Info;
 
 import java.awt.*;
@@ -11,8 +23,14 @@ import java.io.FileNotFoundException;
 
 public class View extends Frame implements ActionListener  {
     /**
-	 MIT lience 
-	 */
+	The GNU General Public License is a free, copyleft license for software and other kinds of works.
+        The licenses for most software and other practical works are designed to take away your freedom to 
+	share and change the works. By contrast, the GNU General Public License is intended to guarantee your 
+	freedom to share and change all versions of a program--to make sure it remains free software for all its 
+	users. We, the Free Software Foundation, 
+	use the GNU General Public License for most of our software; it applies also to any other work released this way by its authors.
+	You can apply it to your programs, too.
+    */
 	private static final long serialVersionUID = 1L;
 	Panel m,in;
     Label ln,lp,r;
